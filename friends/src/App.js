fb5644f2fe92ef196c96b67d39092e6e36f622a5
Rick <rmansfield@realpeoplesratings.com>
2021-08-16 10:33:36 -0500

    correct spelling error on file name Login.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Login from "./components/Login";
+import { axiosWithAuth } from "./utils/axiosWithAuth";
 
 import './App.css';
 
